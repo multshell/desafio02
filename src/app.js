7c52cdc7b7af0adec6455b2ceb980d28dc5bd504
Danilo Costa <danilocostaluiz@hotmail.com>
2020-10-28 18:19:38 -0300

    Challenge Finished

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,81 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const project = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  console.log(project)
+
+  repositories.push(project);
+
+  return response.status(200).json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const projectIndex = repositories.findIndex(project => project.id === id);
+
+  console.log(projectIndex)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not Found." });
+  }
+
+  const project = {
+      id,
+      title,
+      url,
+      techs,
+      likes: repositories[projectIndex].likes
+    }
+
+  repositories[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const projectIndex = repositories.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not Found." });
+  }
+
+  repositories.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const projectIndex = repositories.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not Found." });
+  }
+
+  repositories[projectIndex].likes++;
+
+  project = repositories[projectIndex];
+
+  return response.status(200).json(project);
+
 });
 
 module.exports = app;
